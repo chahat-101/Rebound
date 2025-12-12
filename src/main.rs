0ab40ac72954b11890afbb5ec7fc93f55e6f8980
chahat-101 <chahat-101@users.noreply.github.com>
2025-12-12 22:08:09 +0530

    Empty Window

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,53 @@
+use winit::{
+    event::{Event,WindowEvent},
+    event_loop::{ControlFlow,EventLoop,ActiveEventLoop},
+    window::{Window,WindowId},
+    application::ApplicationHandler,
+    
+};
+#[derive(Default)]
+struct App{
+    window:Option<Window>,
+
+}
+
+
+impl ApplicationHandler for App{
+
+    fn resumed(&mut self, event_loop: & ActiveEventLoop) {
+        
+        let win = event_loop.create_window(Window::default_attributes())
+                            .expect("failed to create window");
+        
+        self.window = Some(win);
+        
+    }
+
+    fn window_event(
+            &mut self,
+            event_loop: &ActiveEventLoop,
+            window_id: WindowId,
+            event: WindowEvent,
+        ) {
+        
+            match event {
+            WindowEvent::CloseRequested => {
+                // drop the window (it closes) and exit the event loop
+                self.window = None;
+                event_loop.exit();
+            }
+            _ => {}
+        }
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+
+    let event_loop = EventLoop::new().unwrap();
+
+    let mut app = App::default();
+
+
+    event_loop.run_app(&mut app).unwrap();
+
 }
