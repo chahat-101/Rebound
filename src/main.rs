d2370786fcd8554d89e597232169aa16cda570aa
chahat-101 <chahat-101@users.noreply.github.com>
2025-12-17 01:51:31 +0530

    2d rectangle

--- src/main.rs
+++ src/main.rs
@@ -1,66 +1,19 @@
-use winit::{
+use macroquad::prelude::*;
 
-    application::ApplicationHandler, 
-    event::{self, Event, WindowEvent}, 
-    event_loop::{self, ActiveEventLoop, ControlFlow, EventLoop}, 
-    window::{self, Window, WindowId}
-};
+struct Wall {
+    rect: Rect,
+}
 
-use pixels::{Pixels,SurfaceTexture};
+#[macroquad::main("Walls")]
+async fn main() {
+    println!("hello world");
+    let wall = Wall {
+        rect: Rect::new(100.0, 200.0, 300.0, 100.0),
+    };
 
-
-
-fn main(){
-    let event_loop = EventLoop::new().unwrap();
-
-   let window = event_loop.create_window(Window::default_attributes()).expect("failed to load window");
-
-    let size = window.inner_size();
-
-    let surface_texture = SurfaceTexture::new(size.width, size.height, &window);
-
-    let mut pixels = Pixels::new(size.width, size.height, surface_texture).unwrap();
-
-
-    event_loop.run(|event,active_event_loop|{
-
-        match event{
-            Event::WindowEvent {event:WindowEvent::CloseRequested,
-                ..
-            } =>{
-                active_event_loop.exit();
-            },
-
-            Event::WindowEvent { 
-                event: WindowEvent::Resized(size),
-                ..
-            } => {
-                pixels.resize_surface(size.width, size.height);
-                pixels.resize_buffer(size.width, size.height);
-                window.request_redraw();
-            },
-
-            Event::WindowEvent { 
-                event:WindowEvent::RedrawRequested,
-                ..
-            } => {
-
-                let frame = pixels.frame_mut();
-
-                for px in frame.chunks_exact_mut(4){
-                    px.copy_from_slice(&[255,255,255,255]);
-                }
-
-                pixels.render().unwrap();
-            },
-
-            _=> {},
-
-        }
-
-       
-
-    });
-
-
+    loop {
+        clear_background(BLACK);
+        draw_rectangle(wall.rect.x, wall.rect.y, wall.rect.w, wall.rect.h, WHITE);
+        next_frame().await;
+    }
 }
